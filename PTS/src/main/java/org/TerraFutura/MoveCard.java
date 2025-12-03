22fdaab2f5320d957abecb33f6cc3334bf1f8712
Kurnava <molnar.peto222@gmail.com>
2025-12-03 18:54:02 +0100

    Added comments to MoveCard

--- PTS/src/main/java/org/TerraFutura/MoveCard.java
+++ PTS/src/main/java/org/TerraFutura/MoveCard.java
@@ -2,21 +2,28 @@ package org.TerraFutura;
 
 import java.util.*;
 
+// Try to move a card from a Pile to the Grid at the given position
+// Returns true if the move succeeded else return false
+
 public class MoveCard {
 
     public boolean moveCard(Pile pile, GridPosition gridCoordinate, Grid grid, int cardIndex) {
 
+        // Validation
         if (pile == null || gridCoordinate == null || grid == null || cardIndex < 0 || cardIndex > 4) {
             return false;
         }
-
         if (!grid.canPutCard(gridCoordinate)) {
             return false;
         }
 
+        // Get the card reference from the pile
         Optional<Card> card = pile.getCard(cardIndex);
 
+        // Remove the card from the pile
         pile.takeCard(cardIndex);
+
+        // Put the card on the grid. If there was no card place null
         grid.putCard(gridCoordinate, card.orElse(null));
 
         return true;
